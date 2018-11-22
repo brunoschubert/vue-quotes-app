d2efb2af11f942e1b44693912f3938dcae94aff6
Bruno Schubert Medeiros <BSCHUBERTM@edu.unisinos.br>
2018-11-22 16:53:02 -0200

    Corrected quote addition

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="row">
         <form>
-          <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 from-group">
-              <label>Quote</label>
-              <textarea class="form-control" rows="3" v-model="quote"></textarea>
-         </div>
-         <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
-              <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
-         </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label>Quote</label>
+                <textarea class="form-control" rows="3" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+               <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+            </div>
         </form>
     </div>
 </template>
@@ -29,4 +29,5 @@
 </script>
 
 <style>
+
 </style>
