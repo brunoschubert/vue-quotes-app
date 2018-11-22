82d928be7643eb139bb546bf253eadc2928b6291
Bruno Schubert Medeiros <BSCHUBERTM@edu.unisinos.br>
2018-11-22 14:38:25 -0200

    Import QuoteGrid to App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="container">
-    
+       <app-quote-grid :quotes="quotes"></app-quote-grid> 
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+
     export default {
         data: function() {
             return {
