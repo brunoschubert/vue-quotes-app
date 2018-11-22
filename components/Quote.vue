5d56c35eb50faa61d55c94717bf63eb61616edf8
Bruno Schubert Medeiros <BSCHUBERTM@edu.unisinos.br>
2018-11-22 14:35:38 -0200

    Add custom template

--- components/Quote.vue
+++ components/Quote.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="row"></div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel panel-default">
+            <div class="panel-body quote">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
